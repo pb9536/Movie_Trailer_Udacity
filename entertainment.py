aeabb9aee114b66feffe3a3c55d070306b7c15c1
pb9536 <pb9536@intl.att.com>
2018-05-11 11:15:17 +0530

    Movie trailer  Database files added to git

---
+++ entertainment.py
@@ -0,0 +1,52 @@
+# Details of instances which displays on browser
+import fresh_tomatoes
+import media
+
+# Create multiple instances
+toy_story = media.Movie("Toy Story",
+                        "Andy's toys get mistakenly delivered to a day care.",
+                        "https://upload.wikimedia.org/wikipedia/en/6/69/Toy_Story_3_poster.jpg",  # NOQA
+                        "https://www.youtube.com/watch?v=6M6ISoW_Gas")
+
+avatar = media.Movie("Avatar",
+                     """Story is about alien world of Pandora
+                        live and humans try to occupy it.""",
+                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",  # NOQA
+                     "https://www.youtube.com/watch?v=d1_JBMrrYw8")
+
+boss_baby = media.Movie("Boss Baby",
+                        """A Little baby can talk and the two team
+                           up to foil the plans of the CEO of Puppy Co.""",
+                        "https://upload.wikimedia.org/wikipedia/en/0/0e/The_Boss_Baby_poster.jpg",  # NOQA
+                        "https://www.youtube.com/watch?v=h24gEn3y82Q")
+
+moana = media.Movie("Moana",
+                    """Moana sets on a voyage to persuade Maui, a demigod
+                       to return the heart of the goddess""",
+                    "https://upload.wikimedia.org/wikipedia/en/2/26/Moana_Teaser_Poster.jpg",  # NOQA
+                    "https://www.youtube.com/watch?v=LKFuXETZUsI")
+
+the_emoji = media.Movie("The Emoji",
+                        """Gene is an emoji that lives in Textopolis,
+                           digital city inside the phone of his user Alex.""",
+                        "https://upload.wikimedia.org/wikipedia/en/6/63/The_Emoji_Movie_film_poster.jpg",  # NOQA
+                        "https://www.youtube.com/watch?v=r8pJt4dK_s4")
+
+avengers = media.Movie("Avengers",
+                       """Avengers is War 2018 American superhero film based
+                          on the Marvel Comics superhero team the Avengers""",
+                       "https://upload.wikimedia.org/wikipedia/en/4/4d/Avengers_Infinity_War_poster.jpg",  # NOQA
+                       "https://www.youtube.com/watch?v=DjNQq0iXJYY")
+
+beauty_beast = media.Movie("Beauty and the Beast",
+                           """Belle, a village girl, embarks on a journey to
+                              save her father from a creature""",
+                           "https://upload.wikimedia.org/wikipedia/en/d/d6/Beauty_and_the_Beast_2017_poster.jpg",  # NOQA
+                           "https://www.youtube.com/watch?v=e3Nl_TCQXuw")
+
+# Store the movie objects in a list
+movies = [toy_story, avatar, boss_baby, moana,
+          the_emoji, beauty_beast, avengers]
+# open the movie website in a browser using a defined module fresh_tomatoes
+
+fresh_tomatoes.open_movies_page(movies)
